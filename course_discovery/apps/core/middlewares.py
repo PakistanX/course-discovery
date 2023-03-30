087de4d97aaab351287d8ee577f3e86f5136a951
Muhammad Usman <muhammad.usman01@arbisoft.com>
2023-03-30 15:58:36 +0500

    Added logs

--- course_discovery/apps/core/middlewares.py
+++ course_discovery/apps/core/middlewares.py
@@ -1,5 +1,8 @@
+import logging
 from django.contrib.sites.middleware import CurrentSiteMiddleware
 from django.urls import reverse
+
+logger = logging.getLogger(__name__)
 
 
 class SkipSiteMiddleware(CurrentSiteMiddleware):
@@ -7,4 +10,5 @@ class SkipSiteMiddleware(CurrentSiteMiddleware):
 
     def process_request(self, request):
         if request.path not in reverse('health'):
+            logger.info('\n\n\nrequest path: {}\n\n\n'.format(request.path))
             return super().process_request(request)
